2ad880760c533e034107ce7768c96c07d29db7cb
Louis <louis.dumasv@gmail.com>
2022-11-23 14:47:17 -0500

    corrected merging problems

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import {Provider} from 'react-redux';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
 
-import HomePage from 'containers/HomePage';
-import DashboardPage from 'containers/DashboardPage';
-import LoginPage from 'containers/LoginPage';
-import RegisterPage from 'containers/RegisterPage';
+import HomePage from "containers/HomePage";
+import DashboardPage from "containers/DashboardPage";
+import LoginPage from "containers/LoginPage";
+import RegisterPage from "containers/RegisterPage";
+import JobPosting from "pages/JobPosting";
 
-import {store} from 'store';
+import { store } from "store";
 
-const App = ()=>(
-	<Provider store={store}>
-		<Router>
-				<Routes>
-					<Route path='/' element={<HomePage />} />
-					<Route path='/dashboard' element={<DashboardPage />} />
-					<Route path='/login' element={<LoginPage />} />
-					<Route path='/register' element={<RegisterPage />} />
-          <Route 
-            path="/jobpostings" 
-            element={<JobPosting />}
-          />
-				</Routes>
-		</Router>
-	</Provider>
+const App = () => (
+  <Provider store={store}>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/jobpostings" element={<JobPosting />} />
+      </Routes>
+    </Router>
+  </Provider>
 );
 
 export default App;
