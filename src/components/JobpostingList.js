2d4c19272ad5e803c5ae5ac1d0280705853121a7
Louis <louis.dumasv@gmail.com>
2022-11-27 18:22:24 -0500

    job posting working still needs validation

--- src/components/JobpostingList.js
+++ src/components/JobpostingList.js
@@ -3,7 +3,9 @@ import React, { useState } from "react";
 import ListGroup from "react-bootstrap/ListGroup";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
+import InputGroup from "react-bootstrap/InputGroup";
 import FormControl from "react-bootstrap/FormControl";
+import Form from "react-bootstrap/Form";
 import {
   MdCheckBox,
   MdCheckBoxOutlineBlank,
@@ -15,13 +17,27 @@ export default function JobpostingList({ jobpostings = [], setJobpostings }) {
   const [show, setShow] = useState(false);
   const [record, setRecord] = useState(null);
 
+  // const {
+  //   id,
+  //   title,
+  //   logo_url,
+  //   location,
+  //   description,
+  //   remote_option,
+  //   employer,
+  // } = record;
+
+  const handleChange = (e) => {
+    setRecord({ ...record, [e.target.name]: e.target.value });
+  };
+
   const handleClose = () => {
     setShow(false);
   };
 
   const handleDelete = (id) => {
     axios
-      .delete(`api/Jobpostings/${id}/`)
+      .delete(`${process.env.REACT_APP_API_URL}/api/postings/${id}/`)
       .then(() => {
         const newJobpostings = jobpostings.filter((t) => {
           return t.id !== id;
@@ -33,9 +49,13 @@ export default function JobpostingList({ jobpostings = [], setJobpostings }) {
       });
   };
 
-  const handleUpdate = async (id, value) => {
-    return axios
-      .patch(`api/Jobpostings/${id}/`, value)
+  const handleUpdate = (id, value) => {
+    console.log(value);
+    axios
+      .patch(
+        `${process.env.REACT_APP_API_URL}/api/postings/default/${id}/`,
+        value.record
+      )
       .then((res) => {
         const { data } = res;
         const newJobpostings = jobpostings.map((t) => {
@@ -59,7 +79,7 @@ export default function JobpostingList({ jobpostings = [], setJobpostings }) {
       >
         <div className="d-flex justify-content-center">
           <span>
-            {t.title} || {t.description} || {t.remote_option}
+            {t.title} || {t.description} || {t.remote_option} || {t.company}
           </span>
         </div>
         <div>
@@ -89,7 +109,7 @@ export default function JobpostingList({ jobpostings = [], setJobpostings }) {
   };
 
   const handleSaveChanges = async () => {
-    await handleUpdate(record.id, { name: record.name });
+    await handleUpdate(record.id, { record });
     handleClose();
   };
 
@@ -101,7 +121,64 @@ export default function JobpostingList({ jobpostings = [], setJobpostings }) {
           <Modal.Title>Edit Jobposting</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <FormControl value={record ? record.name : ""} />
+          <InputGroup className="mb-4">
+            <InputGroup.Text>Title</InputGroup.Text>
+            <FormControl
+              placeholder="enter title"
+              onChange={handleChange}
+              name="title"
+              value={record ? record.title : ""}
+            />
+          </InputGroup>
+          <InputGroup className="mb-4">
+            <InputGroup.Text>Logo Url</InputGroup.Text>
+            <FormControl
+              placeholder="ex. yourHosting/yourImage.ext"
+              onChange={handleChange}
+              name="logo_url"
+              value={record ? record.logo_url : ""}
+            />
+          </InputGroup>
+          <InputGroup className="mb-4">
+            <InputGroup.Text>Location</InputGroup.Text>
+            <FormControl
+              placeholder="ex. Montreal"
+              onChange={handleChange}
+              name="location"
+              value={record ? record.location : ""}
+            />
+          </InputGroup>
+          <InputGroup className="mb-4">
+            <InputGroup.Text>Description</InputGroup.Text>
+            <FormControl
+              placeholder="enter description"
+              onChange={handleChange}
+              name="description"
+              value={record ? record.description : ""}
+            />
+          </InputGroup>
+          <InputGroup className="mb-4">
+            <InputGroup.Text>Company Name</InputGroup.Text>
+            <FormControl
+              placeholder="enter Company Name"
+              onChange={handleChange}
+              name="company"
+              value={record ? record.company : ""}
+            />
+          </InputGroup>
+          <InputGroup className="mb-4">
+            <InputGroup.Text>Remote options</InputGroup.Text>
+            <Form.Select
+              aria-label="Default select example"
+              onChange={handleChange}
+              name="remote_option"
+              value={record ? record.remote_option : ""}
+            >
+              <option value="remote">Remote</option>
+              <option value="hybrid">Hybrid</option>
+              <option value="in-person">In-Person</option>
+            </Form.Select>
+          </InputGroup>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
