5c98f6bda7dcf22c1605d05c91617f0c606b5e21
CocoScrappy <kizyakov.d@gmail.com>
2022-11-29 22:35:25 -0500

    fixing transfer joblist -> applicform w/ zustand

--- src/components/JobApplicationForms/JobApplicationForm.js
+++ src/components/JobApplicationForms/JobApplicationForm.js
@@ -2,31 +2,90 @@ import React, {useState, useRef, useEffect} from 'react'
 import { useLocation } from 'react-router-dom'
 import Layout from 'components/Layout'
 import { Button, InputGroup, Form, Container } from 'react-bootstrap'
-import MyCVsPage from 'containers/MyCVsPage'
+import Heart from "react-heart"
 
-import {useStore} from 'store'
+import MyCVPage from 'containers/MyCVPage'
+
+import useStore from 'store'
+import axios from 'axios'
+
 
 
 function JobApplicationForm({posting}) {
 
+  const state = useStore();
   const location = useLocation();
+
+  const [isLiked, setIsLiked] = useState(false);
+  const [success, setSuccess] = useState(false);
+
+  const [formData, setFormData] = useState({});
+  const {
+    notes
+  } = formData;
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const applicationData = {};
+
+  const submitApplication = async (e) => {
+    e.preventDefault();
+    applicationData = {
+        notes: notes.value,
+        favorited: isLiked,
+        status: "Applied",
+        applicant_id: state.id,
+        cv_id: await MyCVPage.getUserCV().id,
+        job_posting_id: state.jpid,
+      }
+
+    console.log("submitting application")
+    axios.post(`${process.env.REACT_APP_API_URL}/api/applications/`, applicationData)
+          .then((res) => {
+            console.log(res)
+            setSuccess(true)
+          })
+          .catch(() => {
+              alert("Something went wrong while calling database.");
+            });
+
+        setSuccess(true);
+      };
+
 
   return (
 
     <Layout title="JobApplicationForm" content="JobApplicationForm">
-        <div>Job Application Form</div>
+        <div>
+          <h4>Job Application Form</h4>
+        </div>
         <Container>
             <Form>
                 <Form.Label>Company</Form.Label>
-                <Form.Control type="text" placeholder="Company" disabled value={location.state.company} />
+                <Form.Control type='text' disabled value={state.company_name} />
                 <Form.Label>Job Title</Form.Label>
-                <Form.Control type="text" placeholder="Job Title" className="" disabled value={location.state.title}/>
+                <Form.Control type="text" placeholder="Job Title" className="" disabled value={state.title}/>
                 
+                <Form.Label>Favorite</Form.Label>
+                <div style={{ width: "1.5rem" }}>
+                  <Heart isActive={isLiked} onClick={() => setIsLiked(!isLiked)}/>
+                </div>
+
+                <br/>
+
                 <Form.Label>Notes</Form.Label>
-                <Form.Control as="textarea" rows={3} />
+                <Form.Control 
+                    as="textarea" 
+                    rows={3} 
+                    placeholder="Add Notes..."
+                    onChange={handleChange}
+                    name="notes"
+                    value={notes} />
                 {/* Add CV selector <MyCVsPage/> */}
                 
-                <Button variant="primary" type="submit">
+                <Button variant="primary" type="submit" onSubmit={submitApplication} >
                   Apply
                 </Button>
             </Form>
--- src/store.js
+++ src/store.js
@@ -13,6 +13,8 @@ const useStore = create(
       first_name: "",
       last_name: "",
       role: "",
+
+      // User Info
       addId: (uid) => {
         set(() => ({
           id: uid,
@@ -38,11 +40,77 @@ const useStore = create(
           role: uRole,
         }));
       },
+
+
+      jpid: -1,
+      title: "",
+      logo_url: "",
+      location: "",
+      description: "",
+      date_created: "",
+      remote_option: "",
+      employer_id: -1,
+      company_name: "",
+
+      // Job Post (jp) Info
+      addJpId: (jpid) => {
+        set(() => ({
+          jpid: jpid,
+        }));
+      },
+      addTitle: (jpTitle) => {
+        set(() => ({
+          title: jpTitle,
+        }));
+      },
+      addLogoUrl: (jpLogoUrl) => {
+        set(() => ({
+          logo_url: jpLogoUrl,
+        }));
+      },
+      addLocation: (jpLocation) => {
+        set(() => ({
+          location: jpLocation,
+        }));
+      },
+      addDescription: (jpDescription) => {
+        set(() => ({
+          description: jpDescription,
+        }));
+      },
+      addDateCreated: (jpDateCreated) => {
+        set(() => ({
+          date_created: jpDateCreated,
+        }));
+      },
+      addRemoteOption: (jpRemoteOption) => {
+        set(() => ({
+          remote_option: jpRemoteOption,
+        }));
+      },
+      addEmployerId: (jpEmployerId) => {
+        set(() => ({
+          employer_id: jpEmployerId,
+        }));
+      },
+      addCompanyName: (jpCompanyName) => {
+        set(() => ({
+          company_name: jpCompanyName,
+        }));
+      },
+    
+
+
     }),
     {
       name: "User_info",
     }
   )
 );
+
+
+
+
+
 
 export default useStore;
