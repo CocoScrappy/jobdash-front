ce6cfaba26435aa26336818f4130dcb8974aa34c
Alina Gotcherian <alina.gotcherian@gmail.com>
2022-12-02 01:21:38 -0500

    Login/Register forms don't work. WIP grid layout, header + footer, login + reg.

--- src/layouts/LargeBannerLayout.js
+++ src/layouts/LargeBannerLayout.js
@@ -3,7 +3,7 @@ import "../css/layouts/Banners.css";
 import girlwithcomp from "../assets/girlwithcomp.jpeg";
 
 const LargeBannerLayout = ({ children }) => (
-  <div className="container-fluid large-banner d-flex">
+  <div className="container-fluid large-banner d-flex h-100">
     <img
       alt="user avatar"
       src={girlwithcomp}
