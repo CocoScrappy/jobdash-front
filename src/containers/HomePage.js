f0fa6c98db0d09d50e53fed08db672c68c98c707
Alina Gotcherian <alina.gotcherian@gmail.com>
2022-12-07 01:25:53 -0500

    home page

--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -1,9 +1,47 @@
+// Custom Layout
 import Layout from "layouts/MainLayout";
+import LargeBannerLayout from "../layouts/LargeBannerLayout";
+
+// React-Bootstrap
+import Button from "react-bootstrap/Button";
+
+// Css
+import "../css/components/Button.css";
 
 const HomePage = () => {
   return (
     <Layout title="AuthSite | Homepage" content="Homepage">
-      <h1>Home</h1>
+      <LargeBannerLayout>
+        <div
+          className="d-flex flex-column justify-content-around"
+          style={{ height: "400px" }}
+        >
+          <h1 style={{ fontSize: "4rem" }}>The easy way to job search.</h1>
+          <p style={{ fontSize: "1.5rem", opacity: "0.8" }}>
+            All the jobs, all in 1 place. Stay organized and get hired, with
+            Jobdash.
+          </p>
+          <div
+            className="d-flex flex-column flex-lg-row justify-content-between"
+            style={{ gap: "8px" }}
+          >
+            <Button
+              variant="dark"
+              className="shadow-lg btn-jobdash w-100"
+              size="lg"
+            >
+              Login
+            </Button>
+            <Button
+              variant="dark"
+              className="shadow-lg btn-jobdash w-100"
+              size="lg"
+            >
+              Register
+            </Button>
+          </div>
+        </div>
+      </LargeBannerLayout>
     </Layout>
   );
 };
